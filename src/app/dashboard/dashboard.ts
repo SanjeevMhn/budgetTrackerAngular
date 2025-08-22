2a45e0a53ba24c8c40f78f66d56a73f8e3213f0d
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-08-22 13:22:08 +0545

    [FIXED]: fixed theme color and bottom nav rounded in middle

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import { Bell, EllipsisVertical, LucideAngularModule, Plus, Search } from 'lucide-angular';
+import {
+  Bell,
+  CircleArrowDown,
+  CircleArrowUp,
+  EllipsisVertical,
+  History,
+  LucideAngularModule,
+  Plus,
+  Search,
+  Wallet,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-dashboard',
   imports: [LucideAngularModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss'
+  styleUrl: './dashboard.scss',
 })
 export class Dashboard {
-
-  searchIcon = Search
-  bellIcon = Bell
-  dotsIcon = EllipsisVertical
-  addIcon = Plus
+  searchIcon = Search;
+  bellIcon = Bell;
+  dotsIcon = EllipsisVertical;
+  addIcon = Plus;
+  walletIcon = Wallet;
+  refreshIcon = History;
+  circleArrowDownIcon = CircleArrowDown
+  circleArrowUpIcon = CircleArrowUp
 }
