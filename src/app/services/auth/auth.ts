2d11ff5c69e60ecb4278f4fc8ee73a9d1ed630a6
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-09-05 11:42:09 +0545

    [ADDED]: added evironment files for development and production

--- src/app/services/auth/auth.ts
+++ src/app/services/auth/auth.ts
@@ -1,6 +1,7 @@
 import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { UserStore } from '../../store/user-store';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,7 @@ export class Auth {
   userStore = inject(UserStore);
   router = inject(Router);
   ONE_DAY_TOKEN = 24 * 60 * 60 * 1000;
+
   authenticateUser(password: number): void | string {
     if (this.userStore.getUserDetail().password() === password) {
       this.userStore.setUserData({
@@ -28,6 +30,7 @@ export class Auth {
       authenticated: false,
     });
 
-    this.router.navigate(['/login']);
+    // this.router.navigate(['/login']);
+    this.router.navigate([environment.apiUrl])
   }
 }
