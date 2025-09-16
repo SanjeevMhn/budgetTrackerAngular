a26fadf4bca768627fc2e3125ee01443e555bca8
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-09-16 16:51:31 +0545

    [ADDED]: added data export and import feature

--- src/app/dialog/data-settings/data-settings.ts
+++ src/app/dialog/data-settings/data-settings.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { LucideAngularModule, X } from 'lucide-angular';
 import { AlertDialog } from '../alert-dialog/alert-dialog';
-import { TransactionStore } from '../../store/transaction-store';
-import { BudgetStore } from '../../store/budgets-store';
+import { Transaction, TransactionStore } from '../../store/transaction-store';
+import { Budget, BudgetStore } from '../../store/budgets-store';
 import { UserStore } from '../../store/user-store';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { SnackBar } from '../../services/snack-bar';
 
 @Component({
   selector: 'app-data-settings',
@@ -21,11 +22,69 @@ export class DataSettings {
   transactionsStore = inject(TransactionStore);
   budgetsStore = inject(BudgetStore);
   userStore = inject(UserStore);
+  snackBar = inject(SnackBar);
 
   router = inject(Router);
 
   close() {
     this.dialogRef.close();
+  }
+
+  exportData() {
+    let budgets = localStorage.getItem('budgets');
+    let transactions = localStorage.getItem('transactions');
+    let user = localStorage.getItem('user');
+
+    const expenseTrackerData = JSON.stringify({
+      budgets: JSON.parse(budgets!),
+      transactions: JSON.parse(transactions!),
+      user: JSON.parse(user!),
+    });
+
+    const blob = new Blob([expenseTrackerData], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'expenseTracker.json');
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  }
+
+  importedData(event: any) {
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.onload = (e) => {
+      const content = e.target?.result as string;
+      try {
+        const jsonData = JSON.parse(content);
+        const { transactions, budgets, user } = jsonData
+        this.userStore.setUserData({
+          ...user,
+        });
+        if (transactions.transactions.length > 0) {
+          transactions.transactions.forEach((tran:Transaction) => {
+            this.transactionsStore.addTransaction(tran)
+          })
+        }
+        if(budgets.budgets.length > 0){
+          budgets.budgets.forEach((budget: Budget) => {
+            this.budgetsStore.addBudget(budget)
+          })
+        }
+
+        this.snackBar.open('Importing data successful');
+      } catch (err: any) {
+        console.error(err);
+        this.snackBar.open('Error while reading contents of file.');
+      }
+    };
+
+    fileReader.readAsText(file);
   }
 
   deleteReset() {
